8cb7a890b9fe7e4b9029717878e937ed621f359a
Linus Toma <linustoma11@hotmail.com>
2023-12-10 22:17:38 +0100

    Initial commit

---
+++ js/albums.js
@@ -0,0 +1,26 @@
+var albums = [
+    {
+        "id" : 1,
+        "name" : "Rawa Majid",
+        "image": "images/IMG_0609.JPG",
+        "stars": 10,
+        "year": 2023,
+        "genre": "Comedy",
+    },
+    {
+        "id" : 2,
+        "name" : "The Dark Side of the Moon",
+        "image": "images/MV5BMTIzNDYzMzgtZWMzNS00ODc2LTg2ZmMtOTE2MWZkNzIxMmQ0XkEyXkFqcGdeQXVyNjQ3MDg0MTY@._V1_FMjpg_UX1000_.jpg",
+        "stars": 2,
+        "year": 2023,
+        "genre": "Comedy",
+    },
+    {
+        "id" : 3,
+        "name" : "Wish You Were Here",
+        "image": "https://i.scdn.co/image/aab31a87e274822dd11c1de4b6e851aa3a471500",
+        "stars": 7,
+        "year": 2023,
+        "genre": "Comedy",
+    }
+];
---
+++ js/movies.js
@@ -0,0 +1,26 @@
+var movies = [
+    {
+        "id" : 1,
+        "title" : "Rawa Majid",
+        "image": "images/IMG_0609.JPG",
+        "stars": 10,
+        "year": 2023,
+        "genre": "Comedy",
+    },
+    {
+        "id" : 2,
+        "title" : "Pixels",
+        "image": "images/MV5BMTIzNDYzMzgtZWMzNS00ODc2LTg2ZmMtOTE2MWZkNzIxMmQ0XkEyXkFqcGdeQXVyNjQ3MDg0MTY@._V1_FMjpg_UX1000_.jpg",
+        "stars": 5.6,
+        "year": 2015,
+        "genre": "Action/Comedy/Fantasy",
+    },
+    {
+        "id" : 3,
+        "title" : "Harry Potter",
+        "image": "https://miro.medium.com/v2/resize:fit:1200/1*d-Z-MMx5VSZIYsOm36vU-Q.jpeg",
+        "stars": 8.1,
+        "year": 2011,
+        "genre": "Adventure/Family/Fantasy",
+    }
+];
